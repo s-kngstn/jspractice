b67b6ccc8db96c6183a90df34c223797d67212ea
s-kngstn <s.kngstn@protonmail.com>
2021-03-09 16:22:59 +0000

    while loop added - fizzbuzz function

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,19 +1,24 @@
 let output = [];
-let count = 1
+let count = 1;
+
+
 
 function fizzBuzz(){
 
-    if (count % 3 === 0 && count % 5 === 0) {
-        output.push('FizzBuzz');
-    } else if (count % 3 === 0) {
-        output.push('Fizz');
-    } else if (count % 5 === 0) {
-        output.push('Buzz');
-    } else {
-        output.push(count);
-    }
+    while (count <= 100) {
 
-    count++;
-    console.log(output);
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push('FizzBuzz');
+        } else if (count % 3 === 0) {
+            output.push('Fizz');
+        } else if (count % 5 === 0) {
+            output.push('Buzz');
+        } else {
+            output.push(count);
+        }
+
+        count++;
+        console.log(output);
+    }
 
 }
