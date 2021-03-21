730a5bd3bac0e2726b56dc59a82807ffcc26c8ca
s-kngstn <s.kngstn@protonmail.com>
2021-03-21 18:16:37 +0000

    building api

---
+++ api-weather/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get("/", function(req, res){
+    res.send("Server up and running.")
+})
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000")
+})
